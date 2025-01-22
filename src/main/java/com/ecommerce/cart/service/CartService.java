3d2bdc4851427e0b0269d46e0667a59a195c1eef
jeong-yong-shin <shinjy0211@gmail.com>
2025-01-22 21:12:35 +0900

    유저 카트 조회

--- src/main/java/com/ecommerce/cart/service/CartService.java
+++ src/main/java/com/ecommerce/cart/service/CartService.java
@@ -1,19 +1,21 @@
 package com.ecommerce.cart.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ecommerce.cart.DTO.AddItemToCartRequestDto;
 import com.ecommerce.cart.DTO.AddItemToCartResponseDto;
+import com.ecommerce.cart.DTO.CartItemResponseDto;
 import com.ecommerce.cart.model.Cart;
 import com.ecommerce.cartItem.model.CartItem;
+import com.ecommerce.cartItem.projection.CartItemProjection;
 import com.ecommerce.cartItem.repository.CartItemRepository;
 import com.ecommerce.product.model.Product;
 import com.ecommerce.product.service.ProductService;
 import com.ecommerce.user.service.UserService;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -42,5 +44,19 @@ public class CartService {
                 .productName(cartItem.getProduct().getName())
                 .quantity(cartItem.getQuantity()).build();
         return responseDto;
+    }
+
+    public List<CartItemResponseDto> getCartItems(String providerId) {
+        Cart userCart = userService.findCartByProviderid(providerId);
+        List<CartItemProjection> cartItems = cartItemRepository.findCartItemsByCartId(userCart.getId());
+        return cartItems.stream()
+                .map(item -> CartItemResponseDto.builder()
+                        .cartItemId(item.getCartItemId())
+                        .productId(item.getProductId())
+                        .productName(item.getProductName())
+                        .productPrice(item.getProductPrice())
+                        .productQuantity(item.getQuantity())
+                        .build())
+                .toList();
     }
 }
